278283150fae5aa4b3388c24dc217df22bb2738f
dianaaadumitru <dianad2601@gmail.com>
2022-11-16 17:19:59 +0200

    fixes

--- lab3/src/domain/ProgramScanner.java
+++ lab3/src/domain/ProgramScanner.java
@@ -9,79 +9,6 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ProgramScanner {
-//    private ArrayList<String> operators;
-//    private ArrayList<String> separators;
-//    private ArrayList<String> reservedWords;
-//
-//    private ST<String> symbolTable;
-//    private final String pathSourceCode;
-//
-//    public ArrayList<String> getOperators() {
-//        return operators;
-//    }
-//
-//    public ArrayList<String> getSeparators() {
-//        return separators;
-//    }
-//
-//    public ArrayList<String> getReservedWords() {
-//        return reservedWords;
-//    }
-//
-//    public ProgramScanner(String pathSourceCode) {
-//        this.pathSourceCode = pathSourceCode;
-//        this.symbolTable = new ST<>(new HashTable<>(10));
-//        readTokens();
-//    }
-//
-//    private void readTokens() {
-//        try {
-//            Scanner scanner = new Scanner(new File("C:\\Users\\diana\\Desktop\\uni work\\5th sem\\flcd\\FLCD\\lab2\\src\\data\\token.in"));
-//            operators = new ArrayList<>(List.of(scanner.nextLine().split(",")));
-//            separators = new ArrayList<>(List.of(scanner.nextLine().split(",")));
-//            separators.add("\n");
-//            separators.add(",");
-//            reservedWords = new ArrayList<>(List.of(scanner.nextLine().split(",")));
-//        } catch (FileNotFoundException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//
-//    private String readCode() {
-//        StringBuilder code = new StringBuilder();
-//        Scanner scanner;
-//        try {
-//            scanner = new Scanner(new File(this.pathSourceCode));
-//            while (scanner.hasNextLine()) {
-//                code.append(scanner.nextLine()).append("\n");
-//            }
-//            return code.toString().replace("\t", "");
-//        } catch (FileNotFoundException e) {
-//            e.printStackTrace();
-//        }
-//        return null;
-//    }
-//
-//    public List<String> tokenize() {
-//        String code = this.readCode();
-//        String separatorsString = this.separators.toString();
-//
-//        return Collections.list(new StringTokenizer(code, separatorsString, true)).stream()
-//                .map(token -> (String) token)
-//                .collect(Collectors.toList());
-//    }
-//
-//    public boolean isIdentifier(String symbol) {
-//        return symbol.matches("[a-zA-Z][a-zA-Z0-9]*");
-//    }
-//
-//    public boolean isConstant(String symbol) {
-//        if (symbol.matches("[a-zA-Z0-9]*")) {
-//            return true;
-//        }
-//
-//        return false;
-//    }
 
     private String program;
     private List<String> tokens;
@@ -115,14 +42,14 @@ public class ProgramScanner {
         Pattern intRegex = Pattern.compile("^([-]?[1-9]\\d*|0)");
         Matcher matcher = intRegex.matcher(program.substring(index));
         if (matcher.find()) {
-            String token = matcher.group();
+            String token = matcher.group(1);
 
             if (pif.size() > 0) {
                 System.out.println("Found int constant: " + token);
             }
 
-            pif.add(new Pair<>("intConst", new Pair<>(-2, -2)));
-            symbolTable.addToST(token);
+            Pair<Integer, Integer> pair = symbolTable.addToST(token);
+            pif.add(new Pair<>(token, pair));
             index += matcher.end();
             return true;
         }
@@ -136,9 +63,9 @@ public class ProgramScanner {
         Pattern strRegex = Pattern.compile("\"([a-zA-z0-9_ ]*)\"");
         Matcher matcher = strRegex.matcher(program.substring(index));
         if (matcher.find()) {
-            String token = matcher.group();
-            pif.add(new Pair<>("stringConst", new Pair<>(-3, -3)));
-            symbolTable.addToST(token);
+            String token = matcher.group(1);
+            Pair<Integer, Integer> pair = symbolTable.addToST(token);
+            pif.add(new Pair<>(token, pair));
             index += matcher.end();
             System.out.println("Found string constant: " + token);
             return true;
@@ -173,7 +100,7 @@ public class ProgramScanner {
             System.out.println("Found identifier: " + token);
             Pair<Integer, Integer> pair = symbolTable.addToST(token);
             if (pair.getKey() != -1) {
-                pif.add(new Pair<>("id", pair));
+                pif.add(new Pair<>(token, pair));
             }
             index += matcher.end();
             return true;
